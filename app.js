4fbce5bb4c37fe5675ff87fca3eaecd1969924b8
Jeff Button <jeffreyjbutton@gmail.com>
2017-11-01 10:28:16 -0700

    updated app.js

--- app.js
+++ app.js
@@ -1,39 +1,17 @@
 const express = require("express");
 const app = express();
 
-app.use(function(req, res, next) {
-    res.locals.ua = req.get('User-Agent');
-    console.log(res.locals.ua);
-    next();
-  });
+app.get("*", function (req, res) {
+    // const jj = req.headers["user-agent"];
+    // console.log("split jj", jj.split('(')[3].split(')')[0]);
 
-// Route for input
-app.get("/", function (req, res) {
-    res.send("Parser");
+    const jb = {
+        IP: req.headers["x-forwarded-for"].split(',')[0], 
+        language: req.headers["accept-language"].split(',')[0],
+        OS: req.headers["user-agent"].split('(')[1].split(')')[0],
+    }
+    res.send(req.headers);
+    // res.send(jb);
 });
 
-app.get("/:input", function (req, res) {
-    res.send(req.params.input);
-});
-
-exports.logger = function (req, res) {
-    var user = {
-        agent: req.header('user-agent'), // User Agent we get from headers
-        referrer: req.header('referrer'), //  Likewise for referrer
-        ip: req.header('x-forwarded-for') || req.connection.remoteAddress, // Get IP - allow for proxy
-        screen: { // Get screen info that we passed in url post data
-            width: req.param('width'),
-            height: req.param('height')
-        }
-    };
-    // Store the user in your database
-    // User.create(user)...
-    console.log("user", user);
-    res.end();
-}
-
-app.listen(3000, headerParser);
-
-function headerParser() {
-    console.log("Listening on port 3000");
-}
+app.listen(3200, () => console.log("Listening..."));
