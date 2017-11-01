b0154c9c5d3ba95acbe9d0bf08808d64144a45ed
Jeff Button <jeffreyjbutton@gmail.com>
2017-11-01 10:30:00 -0700

    Changed variable name to output and removed console logging

--- app.js
+++ app.js
@@ -2,16 +2,12 @@ const express = require("express");
 const app = express();
 
 app.get("*", function (req, res) {
-    // const jj = req.headers["user-agent"];
-    // console.log("split jj", jj.split('(')[3].split(')')[0]);
-
-    const jb = {
+    const output = {
         IP: req.headers["x-forwarded-for"].split(',')[0], 
         language: req.headers["accept-language"].split(',')[0],
         OS: req.headers["user-agent"].split('(')[1].split(')')[0],
     }
-    res.send(req.headers);
-    // res.send(jb);
+    res.send(output);
 });
 
 app.listen(3200, () => console.log("Listening..."));
